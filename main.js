c8b0a6ee819baaefff9abfffdd9f822c344bb595
William Vitorino <williamvitorino3@gmail.com>
2020-03-18 12:06:33 -0300

    Start

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function main() {
+  
+}
+
+let canvas = document.getElementById('snake')
+let context = canvas.getContext('2d')
+let box = 32
+
+function criarBG () {
+  context.fillStyle = "lightgreen"
+  context.fillRect(0, 0, 16 * box, 16 * box)
+}
+
+criarBG()
